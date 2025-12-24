f06bbc009f1d793f75b56b14d755e17c63e4c4fa
anushb-codes <bhandarianush55@gmail.com>
2025-12-24 12:18:54 +0530

    Create sensor_handler.py

---
+++ mobile-app/sensor_handler.py
@@ -0,0 +1,12 @@
+# MEMBER 1 TASK: SENSOR INTEGRATION
+# ---------------------------------------------------------
+# TODO: Write code to capture Accelerometer & Gyroscope data
+# TODO: Write code to listen for voice commands (Start/Stop)
+
+def get_sensor_data():
+    print("Reading sensor data...")
+    return {"x": 0, "y": 0, "heading": 0}
+
+def listen_for_command():
+    print("Listening for user voice...")
+    return "Navigate to Room 201"
