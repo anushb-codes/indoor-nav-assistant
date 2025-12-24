abaa8b60b5b41c94530bac66b12aeb31f0c7126b
anushb-codes <bhandarianush55@gmail.com>
2025-12-24 12:21:14 +0530

    Create speaker.py

---
+++ voice-service/speaker.py
@@ -0,0 +1,8 @@
+# MEMBER 4 TASK: TEXT-TO-SPEECH
+# ---------------------------------------------------------
+# TODO: Connect to Azure Speech Service
+# TODO: Make the phone "speak" the text string
+
+def speak_text(text):
+    print(f"Speaking now: {text}")
+    # Add Azure TTS code here later
